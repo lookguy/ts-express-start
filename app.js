237a3ba6fc3a2668fc9657f0c2c09dde40b84c33
想飞天的招雄彬 <614619114@qq.com>
2020-09-07 00:43:30 +0800

    2020/9/7

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const Path = require("path")
+const path = require("path")
 
+app.set("views", path.resolve(__dirname, "src"))
+app.set("view engine", "ejs")
 
-app.use('/users/:id', function (req, res, next) {
-    if (req.params.id == 10) {
-        const img = Path.join(__dirname, "src/static/beatiy.webp")
-        res.sendFile(img, { root: "./src/static/beatiy.webp" })
-        res.end()
-    } else {
-        res.send(`userID: ${req.params.id}`)
-    }
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "src")))
+
+app.get("/views", (req, res, next) => {
+    res.render("views", { user: { name: "彬彬" } })
 })
-app.use('/', express.static(Path.join(__dirname, 'src')))
+
+app.use((request, response, next) => {
+    console.log(request.body)
+    console.log(typeof request.body)
+    next()
+})
+
+
+app.use((request, response, next) => {
+    response.end()
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
